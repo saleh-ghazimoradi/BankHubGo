9262c152398204f1007b224b26d18b74e5bf88b3
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-10-30 17:42:51 +0330

    create account, entry, transfer models in the service_model directory

---
+++ internal/service/service_model/entries.go
@@ -0,0 +1,10 @@
+package service_model
+
+import "time"
+
+type Entry struct {
+	ID        int64     `json:"id"`
+	AccountID int64     `json:"account_id"`
+	Amount    int64     `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
+}
