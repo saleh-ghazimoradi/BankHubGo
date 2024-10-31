7f1698f84be836eac5f020cef13a4d8d108f4d8a
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-10-31 08:11:27 +0330

    create some error helper functions in the transport layer for http stuff

---
+++ internal/gateway/errors.go
@@ -0,0 +1,21 @@
+package gateway
+
+import (
+	"github.com/saleh-ghazimoradi/BankHubGo/logger"
+	"net/http"
+)
+
+func internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	logger.Logger.Errorw("internal error", "method", r.Method, "path", r.URL.Path, "err", err.Error())
+	writeJSONError(w, http.StatusInternalServerError, "the server encountered an error")
+}
+
+func badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	logger.Logger.Warnw("bad request", "method", r.Method, "path", r.URL.Path, "err", err.Error())
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
+
+func notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	logger.Logger.Warnf("not found error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
+	writeJSONError(w, http.StatusNotFound, "not found")
+}
