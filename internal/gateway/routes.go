d13477f9a7b23306cc43fdf309ef0f5005251726
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-01 11:28:45 +0330

    implement the account routes in routes.go file

--- internal/gateway/routes.go
+++ internal/gateway/routes.go
@@ -2,14 +2,22 @@ package gateway
 
 import "net/http"
 
-func Routes() http.Handler {
+type Handlers struct {
+	GetAccount    http.HandlerFunc
+	GetAccounts   http.HandlerFunc
+	CreateAccount http.HandlerFunc
+	UpdateAccount http.HandlerFunc
+	DeleteAccount http.HandlerFunc
+}
+
+func Routes(handler Handlers) http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /accounts", nil)
-	mux.HandleFunc("GET /accounts/{id}", nil)
-	mux.HandleFunc("POST /accounts", nil)
-	mux.HandleFunc("PUT /accounts/{id}", nil)
-	mux.HandleFunc("DELETE /accounts/{id}", nil)
+	mux.HandleFunc("GET /accounts", handler.GetAccounts)
+	mux.HandleFunc("GET /accounts/{id}", handler.GetAccount)
+	mux.HandleFunc("POST /accounts", handler.CreateAccount)
+	mux.HandleFunc("PUT /accounts/{id}", handler.UpdateAccount)
+	mux.HandleFunc("DELETE /accounts/{id}", handler.DeleteAccount)
 
 	return mux
 }
