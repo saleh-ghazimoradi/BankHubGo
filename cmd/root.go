2e30dfc2e9254bae8f597bacfdb0aba931951b59
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-01 11:14:00 +0330

    make changes to the config.go file and load it in the root.go file

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,15 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
 package cmd
 
 import (
-	"fmt"
 	"github.com/saleh-ghazimoradi/BankHubGo/config"
 	"github.com/saleh-ghazimoradi/BankHubGo/logger"
-	"log"
 	"os"
 	"time"
 
 	"github.com/spf13/cobra"
-)
-
-var (
-	cfgFile string
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -21,11 +18,10 @@ var rootCmd = &cobra.Command{
 	Short: "A simple bank management system",
 	Long: `BankHubGo is a streamlined bank management service built with Go, 
 designed to introduce core backend development topics and modern tools. 
-This project covers the fundamentals required to build scalable backend services.`,
+This project covers the fundamentals required to build scalable backend services.
+`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := os.Setenv("TZ", time.UTC.String())
 	if err != nil {
@@ -39,19 +35,17 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file path (default is .)")
 	cobra.OnInitialize(
 		initConfig,
-		initLogger, // Logger initialization comes after config
+		initLogger, // logger should come after config
 	)
 }
 
 func initConfig() {
-	err := config.LoadingConfig(cfgFile)
+	err := config.LoadingConfig(".") // Provide the path to app.env
 	if err != nil {
-		log.Fatalf("Could not load config: %v", err)
+		logger.Logger.Fatal("failed to load configuration: ", err)
 	}
-	fmt.Printf("Configuration loaded: %+v\n", config.Appconfig) // Verify config
 }
 
 func initLogger() {
--- config/config.go
+++ config/config.go
@@ -8,25 +8,21 @@ import (
 var Appconfig *Config
 
 type Config struct {
-	DBDriver      string     `mapstructure:"DB_DRIVER"`
-	DBSource      string     `mapstructure:"DB_SOURCE"`
-	ServerAddress string     `mapstructure:"SERVER_ADDRESS"`
-	PostConfig    PostConfig `mapstructure:"POST_CONFIG"`
-	LogLevel      int8       `mapstructure:"LOG_LEVEL"`
-	Env           string     `mapstructure:"ENV"`
-}
-
-type PostConfig struct {
-	DBHost       string        `mapstructure:"DB_HOST"`
-	DBPort       string        `mapstructure:"DB_PORT"`
-	DBUser       string        `mapstructure:"POSTGRES_USER"`
-	DBPassword   string        `mapstructure:"POSTGRES_PASSWORD"`
-	DBName       string        `mapstructure:"POSTGRES_DB"`
-	DBSSLMode    string        `mapstructure:"DB_SSLMODE"`
-	MaxOpenConns int           `mapstructure:"MAX_OPEN_CONNS"`
-	MaxIdleConns int           `mapstructure:"MAX_IDLE_CONNS"`
-	MaxIdleTime  time.Duration `mapstructure:"MAX_IDLE_TIME"`
-	Timeout      time.Duration `mapstructure:"TIMEOUT"`
+	DBDriver      string        `mapstructure:"DB_DRIVER"`
+	DBSource      string        `mapstructure:"DB_SOURCE"`
+	ServerAddress string        `mapstructure:"SERVER_ADDRESS"`
+	LogLevel      int8          `mapstructure:"LOG_LEVEL"`
+	Env           string        `mapstructure:"ENV"`
+	DBHost        string        `mapstructure:"DB_HOST"`
+	DBPort        string        `mapstructure:"DB_PORT"`
+	DBUser        string        `mapstructure:"POSTGRES_USER"`
+	DBPassword    string        `mapstructure:"POSTGRES_PASSWORD"`
+	DBName        string        `mapstructure:"POSTGRES_DB"`
+	DBSSLMode     string        `mapstructure:"DB_SSLMODE"`
+	MaxOpenConns  int           `mapstructure:"MAX_OPEN_CONNS"`
+	MaxIdleConns  int           `mapstructure:"MAX_IDLE_CONNS"`
+	MaxIdleTime   time.Duration `mapstructure:"MAX_IDLE_TIME"`
+	Timeout       time.Duration `mapstructure:"TIMEOUT"`
 }
 
 func LoadingConfig(path string) error {
@@ -35,14 +31,12 @@ func LoadingConfig(path string) error {
 	viper.SetConfigType("env")
 	viper.AutomaticEnv()
 
-	err := viper.ReadInConfig()
-	if err != nil {
+	if err := viper.ReadInConfig(); err != nil {
 		return err
 	}
 
 	var config Config
-	err = viper.Unmarshal(&config)
-	if err != nil {
+	if err := viper.Unmarshal(&config); err != nil {
 		return err
 	}
 
