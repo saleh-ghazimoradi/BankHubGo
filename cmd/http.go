fc6b24664b2c6f4ae641f659df82fe52a4d2b5d5
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2024-11-01 11:18:04 +0330

    improve http.go in the cmd directory by adding server and exposing the handlers

--- cmd/http.go
+++ cmd/http.go
@@ -4,30 +4,57 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/saleh-ghazimoradi/BankHubGo/config"
+	"github.com/saleh-ghazimoradi/BankHubGo/internal/gateway"
+	"github.com/saleh-ghazimoradi/BankHubGo/internal/repository"
+	"github.com/saleh-ghazimoradi/BankHubGo/internal/service"
+	"github.com/saleh-ghazimoradi/BankHubGo/logger"
+	utils "github.com/saleh-ghazimoradi/BankHubGo/utils/connections"
 	"github.com/spf13/cobra"
 )
 
-// httpCmd represents the http command
 var httpCmd = &cobra.Command{
 	Use:   "http",
 	Short: "launching the http rest listen server",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("http called")
+		logger.Logger.Infow("server has started", "addr", config.Appconfig.ServerAddress, "env", config.Appconfig.Env)
+
+		cfg := utils.PostConfig{
+			Host:         config.Appconfig.DBHost,
+			Port:         config.Appconfig.DBPort,
+			User:         config.Appconfig.DBUser,
+			Password:     config.Appconfig.DBPassword,
+			Database:     config.Appconfig.DBName,
+			SSLMode:      config.Appconfig.DBSSLMode,
+			MaxIdleTime:  config.Appconfig.MaxIdleTime,
+			MaxIdleConns: config.Appconfig.MaxIdleConns,
+			MaxOpenConns: config.Appconfig.MaxOpenConns,
+			Timeout:      config.Appconfig.Timeout,
+		}
+
+		db, err := utils.PostConnection(cfg)
+		if err != nil {
+			logger.Logger.Fatal(err)
+		}
+		defer db.Close()
+
+		logger.Logger.Infow("Postgresql connection pool established")
+
+		accountDB := repository.NewAccountRepository(db)
+		accountService := service.NewAccountService(accountDB)
+		accountHandler := gateway.NewAccountHandler(accountService)
+
+		routeHandlers := gateway.Handlers{
+			GetAccount: accountHandler.GetAccount,
+		}
+
+		if err := gateway.Server(gateway.Routes(routeHandlers)); err != nil {
+			logger.Logger.Fatal(err)
+		}
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(httpCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// httpCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// httpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
